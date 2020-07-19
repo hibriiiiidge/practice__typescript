e745700746738458cf35f8391382728861ec623e
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-07-19 09:22:09 +0900

    section2-18

---
+++ section2/index.ts
@@ -0,0 +1,30 @@
+let hasValue = true;
+let count: number = 10;
+let float: number = 3.14;
+let minus: number = -10;
+let single: string = 'hello';
+let hello: string;
+hello = 'hello'
+
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 22
+}
+
+const person2 = {
+  name: {
+    first: 'Jack',
+    last: 'Smith'
+  },
+  age: 22,
+};
+
+// array
+const fruits: string[] = ['Apple', 'Banana', 'Grape'];
+
+// tuple
+const book: [string, number, boolean] = ['business', 1500, false];
