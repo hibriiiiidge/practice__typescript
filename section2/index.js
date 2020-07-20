3b3a5272aa259942659df224494bde82cc44d928
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-07-20 21:56:59 +0900

    section2-32

--- section2/index.js
+++ section2/index.js
@@ -1,5 +1,6 @@
 var hasValue = true;
 var count = 10;
+// count = 'hello'
 var float = 3.14;
 var minus = -10;
 var single = 'hello';
@@ -33,3 +34,90 @@ var coffee = {
 };
 coffee.size = CoffeeSize.SHORT;
 console.log(CoffeeSize.GRANDE);
+var anything = true;
+anything = 'hello';
+var banana = 'banana';
+banana = anything;
+// any の型は banana という String にも入れれる
+var unionType = 10;
+// unionType.toUpperCase();
+unionType = 'hello';
+unionType.toUpperCase();
+var unionTypes = [21, 'hello'];
+var apple = 'apple';
+// リテラル型 const であることがポイント
+var lemon = 'lemon';
+var clothSize = "small";
+// enum みたいに使える
+var cloth = {
+    color: "white",
+    size: clothSize
+};
+cloth.size = 'large';
+function add2(num1, num2) {
+    return num1 + num2;
+}
+function sayHello2() {
+    console.log('Hello!');
+    return;
+}
+// return がある場合のみ undefined が使える
+function sayHello3() {
+    console.log("Hello!");
+    return;
+}
+console.log(sayHello2());
+// 関数の型注釈は矢印 =>
+// 関数の型定義はコロン :
+var anotherAdd = add2;
+// 無名関数
+var anotherAdd2 = function (num1, num2) {
+    return num1 * num2;
+};
+var anotherAdd3 = function (num1, num2) {
+    return num1 * num2;
+};
+var anotherAdd4 = function (num1, num2) {
+    return num1 * num2;
+};
+// アロー関数
+// 1
+var doubleNumber1 = function (num) { return num * 2; };
+var doubleNumber1_1 = function (num) {
+    return num * 2;
+};
+// 2
+var doubleNumber2 = function (num) { return num * 2; };
+// 3
+var doubleNumber3 = function (num) { return num * 2; };
+// callback
+function doubleAndHandle(num, cb) {
+    var doubleNum = cb(num * 2);
+    console.log(doubleNum);
+}
+doubleAndHandle(21, function (doubleNum) {
+    return doubleNum;
+});
+// callback が void => 何も返さない
+function doubleAndHandle2(num, cb) {
+    var doubleNum = cb(num * 2);
+    // if(doubleNum)
+    console.log(doubleNum);
+}
+doubleAndHandle2(21, function (doubleNum) {
+    return doubleNum;
+});
+// unknown
+var unknownInput;
+var anyInput;
+var text;
+// text = unknownInput;
+text = anyInput;
+if (typeof unknownInput === 'string') {
+    text = unknownInput;
+}
+// never
+function error(message) {
+    throw new Error(message);
+}
+// void は undefined を返す
--- section2/index.ts
+++ section2/index.ts
@@ -1,5 +1,6 @@
 let hasValue = true;
 let count: number = 10;
+// count = 'hello'
 let float: number = 3.14;
 let minus: number = -10;
 let single: string = 'hello';
@@ -44,3 +45,123 @@ const coffee = {
 coffee.size = CoffeeSize.SHORT;
 console.log(CoffeeSize.GRANDE);
 
+let anything: any = true;
+anything = 'hello';
+
+let banana = 'banana';
+banana = anything;
+// any の型は banana という String にも入れれる
+
+let unionType: number | string = 10;
+// unionType.toUpperCase();
+unionType = 'hello'
+unionType.toUpperCase();
+let unionTypes: (number | string)[] = [21, 'hello']
+
+
+const apple = 'apple';
+// リテラル型 const であることがポイント
+let lemon = 'lemon';
+
+type ClothSize = 'small' | 'medium' | 'large'
+
+let clothSize: ClothSize = "small";
+// enum みたいに使える
+
+const cloth: {
+  color: string;
+  size: ClothSize;
+} = {
+  color: "white",
+  size: clothSize,
+};
+
+cloth.size = 'large'
+
+function add2(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function sayHello2(): void {
+  console.log('Hello!');
+  return;
+}
+
+// return がある場合のみ undefined が使える
+function sayHello3(): undefined {
+  console.log("Hello!");
+  return;
+}
+
+console.log(sayHello2());
+
+// 関数の型注釈は矢印 =>
+// 関数の型定義はコロン :
+const anotherAdd: (n1: number, n2: number) => number = add2;
+
+// 無名関数
+const anotherAdd2: (n1: number, n2: number) => number = function (num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+const anotherAdd3 = function (num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+const anotherAdd4: (n1: number, n2: number) => number = function (num1, num2) {
+  return num1 * num2;
+}
+
+
+// アロー関数
+// 1
+const doubleNumber1 = num => num * 2
+const doubleNumber1_1 = function (num) {
+  return num * 2;
+}
+
+// 2
+const doubleNumber2 = (num: number): number => num * 2;
+
+
+// 3
+const doubleNumber3: (num: number) => number = num => num * 2;
+
+// callback
+function doubleAndHandle(num: number, cb: (num: number) => number): void {
+  const doubleNum = cb(num * 2);
+  console.log(doubleNum);
+}
+doubleAndHandle(21, doubleNum => {
+  return doubleNum;
+});
+
+// callback が void => 何も返さない
+function doubleAndHandle2(num: number, cb: (num: number) => void): void {
+  const doubleNum = cb(num * 2);
+  // if(doubleNum)
+  console.log(doubleNum);
+}
+doubleAndHandle2(21, (doubleNum) => {
+  return doubleNum;
+});
+
+
+// unknown
+let unknownInput: unknown;
+let anyInput: any;
+let text: string;
+
+// text = unknownInput;
+text = anyInput;
+
+if (typeof unknownInput === 'string') {
+  text = unknownInput;
+}
+
+
+// never
+function error(message: string): never {
+  throw new Error(message);
+}
+// void は undefined を返す
